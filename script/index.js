5f812132f3367f04a640cfc044522be3dded8e3f
Alexis-1709 <alexis.duquenne@freyja.software>
2023-02-16 13:32:21 +0100

    init repo

---
+++ script/index.js
@@ -0,0 +1,203 @@
+const db = require('better-sqlite3')('./snapshot.db');
+const toUi = Math.pow(10, -9);
+var fs = require('fs');
+const axios = require('axios');
+
+var data = {}
+var owners = []
+var dataMarinadeQuarryBalance = []
+var dataMSolHolders = []
+var dataCMSolHolders = []
+var dataFriktionHolders = []
+var dataRaydiumV2Holders = []
+var dataPortHolders = []
+var dataOrcaAquafarmsHolders = []
+
+
+async function main(){
+    console.log("start");
+    console.info(Date.now())
+    await getMSolHolders();
+    //await getMarinadeQuarryBalance();
+    await getCMSolHolders();
+    await getFriktionBalance();
+    await getRaydiumV2Balance();
+    await getPortBalance();
+    await getOrcaAquafarmsBalance();
+    parseData();
+    fs.writeFile('data.json',JSON.stringify(data, null, 2) , 'utf8', function (err) {
+        if (err) throw err;
+        console.log('complete');
+    });
+    //console.log(JSON.stringify(data, null, 2));
+
+
+
+    console.info(Date.now())
+    console.log("end");
+    closeDB();
+}
+main();
+
+// async function getMarinadeQuarryBalance() {
+//     const result = db.prepare(`SELECT token_account.owner, token_account.amount, account.pubkey FROM token_account, account WHERE token_account.mint= ? AND token_account.owner=account.pubkey AND account.owner= ? ORDER BY amount desc`).all(["mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So", "QMNeHCGYnLVDn1icRAfQZpjPLBNkfGbSKRB83G5d8KB"]);
+//     result.forEach((row) => {
+//         dataMarinadeQuarryBalance[row.owner] = row.amount*toUi;
+//         if(owners.indexOf(row.owner) == -1){
+//             owners.push(row.owner);
+//         }
+//     });
+//     // result.forEach((row) => {
+//     //     dataMSolHolders.push({amount : row.amount*toUi, owner: row.owner});
+//     // });
+// }
+
+async function getMSolHolders() {
+    const result = db.prepare(`SELECT token_account.owner, token_account.amount, account.pubkey FROM token_account, account WHERE token_account.mint= ? AND token_account.owner=account.pubkey AND account.owner= ? AND token_account.amount>0 ORDER BY token_account.amount desc`).all(["mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So", "11111111111111111111111111111111"]);
+    result.forEach((row) => {
+        if(dataMSolHolders[row.owner] == undefined){
+            dataMSolHolders[row.owner] = row.amount*toUi;
+        }else{
+            dataMSolHolders[row.owner] += row.amount*toUi;
+        }
+
+        if(owners.indexOf(row.owner) == -1){
+            owners.push(row.owner);
+        }
+    });
+}
+
+
+async function getCMSolHolders() {
+    const result = db.prepare(`SELECT owner, amount, pubkey FROM token_account WHERE mint= ? AND amount>0 ORDER BY amount desc`).all("3JFC4cB56Er45nWVe29Bhnn5GnwQzSmHVf6eUq9ac91h");
+
+    result.forEach((row) => {
+        if(dataCMSolHolders[row.owner] == undefined){
+            dataCMSolHolders[row.owner] = row.amount*toUi;
+        }else{
+            dataCMSolHolders[row.owner] += row.amount*toUi;
+        }
+
+        if(owners.indexOf(row.owner) == -1){
+            owners.push(row.owner);
+        }
+    });
+
+}
+
+async function getFriktionBalance(){
+    const result = db.prepare(`SELECT token_account.owner, token_account.amount, account.pubkey FROM token_account, account WHERE token_account.mint= ? AND token_account.owner=account.pubkey AND account.owner= ? AND token_account.amount>0 ORDER BY token_account.amount desc`).all(["6UA3yn28XecAHLTwoCtjfzy3WcyQj1x13bxnH8urUiKt", "11111111111111111111111111111111"]);
+    result.forEach((row) => {
+        if(dataFriktionHolders[row.owner] == undefined){
+            dataFriktionHolders[row.owner] = row.amount*toUi;
+        }else{
+            dataFriktionHolders[row.owner] += row.amount*toUi;
+        }
+
+        if(owners.indexOf(row.owner) == -1){
+            owners.push(row.owner);
+        }
+    });
+}
+
+async function getOrcaAquafarmsBalance(){
+    const mSOL_USDT_supply = db.prepare(`SELECT supply FROM token_mint WHERE pubkey= ?`).all("Afvh7TWfcT1E9eEEWJk17fPjnqk36hreTJJK5g3s4fm8")[0].supply*toUi;
+    const mSOL_in_liq_USDT = db.prepare(`SELECT amount FROM token_account WHERE pubkey= ?`).all("RTXKRxghfWJpE344UG7UhKnCwN2Gyv6KnNSTFDnaASF")[0].amount*toUi;
+    const mSOL_per_LP_USDT = mSOL_in_liq_USDT/mSOL_USDT_supply;
+    const result = db.prepare(`SELECT token_account.owner, token_account.amount, account.pubkey FROM token_account, account WHERE token_account.mint= ? AND token_account.owner=account.pubkey AND account.owner= ? AND token_account.amount>0 ORDER BY token_account.amount desc`).all(["Afvh7TWfcT1E9eEEWJk17fPjnqk36hreTJJK5g3s4fm8", "11111111111111111111111111111111"]);
+    const resultDoubleDip = db.prepare(`SELECT token_account.owner, token_account.amount, account.pubkey FROM token_account, account WHERE token_account.mint= ? AND token_account.owner=account.pubkey AND account.owner= ? AND token_account.amount>0 ORDER BY token_account.amount desc`).all(["7iKG16aukdXXw43MowbfrGqXhAoYe51iVR9u2Nf2dCEY", "11111111111111111111111111111111"]);
+    result.forEach((row) => {
+        if(dataOrcaAquafarmsHolders[row.owner] == undefined){
+            dataOrcaAquafarmsHolders[row.owner] = {mSOL_USDT: row.amount*toUi*mSOL_per_LP_USDT};
+        }else{
+            dataOrcaAquafarmsHolders[row.owner].mSOL_USDT += row.amount*toUi*mSOL_per_LP_USDT;
+        }
+
+        if(owners.indexOf(row.owner) == -1){
+            owners.push(row.owner);
+        }
+    });
+    resultDoubleDip.forEach((row) => {
+        if(dataOrcaAquafarmsHolders[row.owner] == undefined){
+            dataOrcaAquafarmsHolders[row.owner] = {mSOL_USDT: row.amount*toUi};
+        }else{
+            dataOrcaAquafarmsHolders[row.owner].mSOL_USDT += row.amount*toUi;
+        }
+
+        if(owners.indexOf(row.owner) == -1){
+            owners.push(row.owner);
+        }
+    });
+}
+
+async function getRaydiumV2Balance(){
+    const mSOL_USDC_supply = db.prepare(`SELECT supply FROM token_mint WHERE pubkey= ?`).all("4xTpJ4p76bAeggXoYywpCCNKfJspbuRzZ79R7pRhbqSf")[0].supply*toUi;
+    const mSOL_in_liq_USDC = db.prepare(`SELECT amount FROM token_account WHERE pubkey= ?`).all("8JUjWjAyXTMB4ZXcV7nk3p6Gg1fWAAoSck7xekuyADKL")[0].amount*toUi;
+    const mSOL_per_LP_USDC = mSOL_in_liq_USDC/mSOL_USDC_supply;
+    const result = db.prepare(`SELECT token_account.owner, token_account.amount, account.pubkey FROM token_account, account WHERE token_account.mint= ? AND token_account.owner=account.pubkey AND account.owner= ? AND token_account.amount>0 ORDER BY token_account.amount desc`).all(["4xTpJ4p76bAeggXoYywpCCNKfJspbuRzZ79R7pRhbqSf", "11111111111111111111111111111111"]);
+    result.forEach((row) => {
+        if(dataRaydiumV2Holders[row.owner] == undefined){
+            dataRaydiumV2Holders[row.owner] = {mSOL_USDC: row.amount*toUi*mSOL_per_LP_USDC, mSOL_SOL: 0};
+        }else{
+            dataRaydiumV2Holders[row.owner].mSOL_USDC += row.amount*toUi*mSOL_per_LP_USDC;
+        }
+
+        if(owners.indexOf(row.owner) == -1){
+            owners.push(row.owner);
+        }
+    });
+
+    const mSOL_SOL_supply = db.prepare(`SELECT supply FROM token_mint WHERE pubkey= ?`).all("5ijRoAHVgd5T5CNtK5KDRUBZ7Bffb69nktMj5n6ks6m4")[0].supply*toUi;
+    const mSOL_in_liq_SOL = db.prepare(`SELECT amount FROM token_account WHERE pubkey= ?`).all("85SxT7AdDQvJg6pZLoDf7vPiuXLj5UYZLVVNWD1NjnFK")[0].amount*toUi;
+    const mSOL_per_LP_SOL = mSOL_in_liq_SOL/mSOL_SOL_supply;
+    const result2 = db.prepare(`SELECT token_account.owner, token_account.amount, account.pubkey FROM token_account, account WHERE token_account.mint= ? AND token_account.owner=account.pubkey AND account.owner= ? AND token_account.amount>0 ORDER BY token_account.amount desc`).all(["5ijRoAHVgd5T5CNtK5KDRUBZ7Bffb69nktMj5n6ks6m4", "11111111111111111111111111111111"]);
+    result2.forEach((row) => {
+        if(dataRaydiumV2Holders[row.owner] == undefined){
+            dataRaydiumV2Holders[row.owner] = {mSOL_USDC: 0, mSOL_SOL: row.amount*toUi*mSOL_per_LP_SOL};
+        }else{
+            dataRaydiumV2Holders[row.owner].mSOL_SOL += row.amount*toUi*mSOL_per_LP_SOL;
+        }
+
+        if(owners.indexOf(row.owner) == -1){
+            owners.push(row.owner);
+        }
+    });
+}
+
+async function getPortBalance(){
+    const portData = await axios.get("https://api-v1.port.finance/collaterals/msol").then((response) => {
+        const data = response.data;
+        data.forEach((user) => {
+            dataPortHolders[user.owner] = Number(user.uiAmount);
+            if(owners.indexOf(user.owner) == -1){
+                owners.push(user.owner);
+            }
+        });
+    });
+}
+
+
+
+function parseData(){
+    owners.forEach((owner) => {
+        data[owner] = {
+            walletBalance : dataMSolHolders[owner] ? dataMSolHolders[owner] : 0,
+            marinadeQuarryBalance : dataMarinadeQuarryBalance[owner] ? dataMarinadeQuarryBalance[owner] : 0,
+            solendBalance : dataCMSolHolders[owner] ? dataCMSolHolders[owner] : 0,
+            friktionBalance : dataFriktionHolders[owner] ? dataFriktionHolders[owner] : 0,
+            raydiumV2Balance : dataRaydiumV2Holders[owner] ? dataRaydiumV2Holders[owner] : {mSOL_USDC: 0, mSOL_SOL: 0},
+            portBalance : dataPortHolders[owner] ? dataPortHolders[owner] : 0,
+            OrcaAquafarmsBalance : dataOrcaAquafarmsHolders[owner] ? dataOrcaAquafarmsHolders[owner] : {mSOL_USDT: 0},
+        }
+    })
+}
+
+function closeDB() {
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Close the database connection.');
+    });
+}
+
---
+++ snapshot-etl-fork/src/bin/solana-snapshot-fork/sqlite.rs
@@ -0,0 +1,485 @@
+use borsh::BorshDeserialize;
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use log::{error/*,warn*/};
+use rusqlite::{params, Connection};
+use solana_sdk::program_pack::Pack;
+use solana_snapshot_fork::append_vec::{AppendVec, StoredAccountMeta};
+use solana_snapshot_fork::parallel::{AppendVecConsumer, GenericResult};
+use solana_snapshot_fork::{append_vec_iter, AppendVecIterator};
+use std::path::{Path, PathBuf};
+use std::rc::Rc;
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::sync::{Arc, Mutex};
+
+use crate::mpl_metadata;
+
+pub(crate) type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+pub(crate) struct SqliteIndexer {
+    db: Connection,
+    db_path: PathBuf,
+    db_temp_guard: TempFileGuard,
+
+    multi_progress: MultiProgress,
+    progress: Arc<Progress>,
+}
+
+struct Progress {
+    accounts_counter: ProgressCounter,
+    token_accounts_counter: ProgressCounter,
+    metaplex_accounts_counter: ProgressCounter,
+}
+
+pub(crate) struct IndexStats {
+    pub(crate) accounts_total: u64,
+    pub(crate) token_accounts_total: u64,
+    pub(crate) metaplex_accounts_total: u64,
+}
+
+impl SqliteIndexer {
+    pub(crate) fn new(db_path: PathBuf) -> Result<Self> {
+        // Create temporary DB file, which gets promoted on success.
+        let temp_file_name = format!("_{}.tmp", db_path.file_name().unwrap().to_string_lossy());
+        let db_temp_path = db_path.with_file_name(&temp_file_name);
+        let _ = std::fs::remove_file(&db_temp_path);
+        let db_temp_guard = TempFileGuard::new(db_temp_path.clone());
+
+        // Open database.
+        let db = Self::create_db(&db_temp_path)?;
+
+        // Create progress bars.
+        let spinner_style = ProgressStyle::with_template(
+            "{prefix:>13.bold.dim} {spinner} rate={per_sec:>13} total={human_pos:>11}",
+        )
+        .unwrap();
+        let multi_progress = MultiProgress::new();
+        let accounts_counter = ProgressCounter::new(
+            multi_progress.add(
+                ProgressBar::new_spinner()
+                    .with_style(spinner_style.clone())
+                    .with_prefix("accs"),
+            ),
+        );
+        let token_accounts_counter = ProgressCounter::new(
+            multi_progress.add(
+                ProgressBar::new_spinner()
+                    .with_style(spinner_style.clone())
+                    .with_prefix("token_accs"),
+            ),
+        );
+        let metaplex_accounts_counter = ProgressCounter::new(
+            multi_progress.add(
+                ProgressBar::new_spinner()
+                    .with_style(spinner_style.clone())
+                    .with_prefix("metaplex_accs"),
+            ),
+        );
+
+        Ok(Self {
+            db,
+            db_path,
+            db_temp_guard,
+
+            multi_progress,
+            progress: Arc::new(Progress {
+                accounts_counter,
+                token_accounts_counter,
+                metaplex_accounts_counter,
+            }),
+        })
+    }
+
+    fn create_db(path: &Path) -> Result<Connection> {
+        let db = Connection::open(&path)?;
+        db.pragma_update(None, "synchronous", false)?;
+        db.pragma_update(None, "journal_mode", "off")?;
+        db.pragma_update(None, "locking_mode", "exclusive")?;
+        db.execute(
+            "\
+CREATE TABLE account  (
+    pubkey TEXT NOT NULL PRIMARY KEY,
+    data_len INTEGER(8) NOT NULL,
+    owner TEXT NOT NULL,
+    lamports INTEGER(8) NOT NULL,
+    executable INTEGER(1) NOT NULL,
+    rent_epoch INTEGER(8) NOT NULL
+);",
+            [],
+        )?;
+        db.execute(
+            "\
+CREATE TABLE token_mint (
+    pubkey TEXT NOT NULL PRIMARY KEY,
+    mint_authority TEXT NULL,
+    supply INTEGER(8) NOT NULL,
+    decimals INTEGER(2) NOT NULL,
+    is_initialized BOOL NOT NULL,
+    freeze_authority TEXT NULL
+);",
+            [],
+        )?;
+        db.execute(
+            "\
+CREATE TABLE token_account (
+    pubkey TEXT NOT NULL PRIMARY KEY,
+    mint TEXT NOT NULL,
+    owner TEXT NOT NULL,
+    amount INTEGER(8) NOT NULL,
+    delegate TEXT,
+    state INTEGER(1) NOT NULL,
+    is_native INTEGER(8),
+    delegated_amount INTEGER(8) NOT NULL,
+    close_authority TEXT
+);",
+            [],
+        )?;
+//         db.execute(
+//             "\
+// CREATE TABLE token_multisig (
+//     pubkey TEXT NOT NULL,
+//     signer TEXT NOT NULL,
+//     m INTEGER(2) NOT NULL,
+//     n INTEGER(2) NOT NULL,
+//     PRIMARY KEY (pubkey, signer)
+// );",
+//             [],
+//         )?;
+        db.execute(
+            "\
+CREATE TABLE token_metadata (
+    pubkey TEXT NOT NULL,
+    mint TEXT NOT NULL,
+    update_authority TEXT NOT NULL,
+    name TEXT NOT NULL,
+    symbol TEXT(10) NOT NULL,
+    uri TEXT(200) NOT NULL,
+    seller_fee_basis_points INTEGER(4) NOT NULL,
+    primary_sale_happened INTEGER(1) NOT NULL,
+    is_mutable INTEGER(1) NOT NULL,
+    edition_nonce INTEGER(2) NULL,
+    collection_verified INTEGER(1) NULL,
+    collection_key TEXT NULL
+);",
+            [],
+        )?;
+        Ok(db)
+    }
+
+    pub(crate) fn set_cache_size(&mut self, size_mib: i64) -> Result<()> {
+        let size = size_mib * 1024;
+        self.db.pragma_update(None, "cache_size", -size)?;
+        Ok(())
+    }
+
+    pub(crate) fn insert_all(mut self, iterator: AppendVecIterator) -> Result<IndexStats> {
+        let mut worker = Worker {
+            db: &self.db,
+            progress: Arc::clone(&self.progress),
+        };
+        for append_vec in iterator {
+            worker.on_append_vec(append_vec?)?;
+        }
+        self.db.pragma_update(None, "query_only", true)?;
+        let stats = IndexStats {
+            accounts_total: self.progress.accounts_counter.get(),
+            token_accounts_total: self.progress.token_accounts_counter.get(),
+            metaplex_accounts_total: self.progress.metaplex_accounts_counter.get(),
+        };
+        self.db_temp_guard.promote(self.db_path)?;
+        let _ = &self.multi_progress;
+        Ok(stats)
+    }
+}
+
+struct Worker<'a> {
+    db: &'a Connection,
+    progress: Arc<Progress>,
+}
+
+impl<'a> AppendVecConsumer for Worker<'a> {
+    fn on_append_vec(&mut self, append_vec: AppendVec) -> GenericResult<()> {
+        for acc in append_vec_iter(Rc::new(append_vec)) {
+            self.insert_account(&acc.access().unwrap())?;
+        }
+        Ok(())
+    }
+}
+
+impl<'a> Worker<'a> {
+    fn insert_account(&mut self, account: &StoredAccountMeta) -> Result<()> {
+        let filter_account_owners = "QMNeHCGYnLVDn1icRAfQZpjPLBNkfGbSKRB83G5d8KB,11111111111111111111111111111111";
+        let filter_account_mints = "mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So,3JFC4cB56Er45nWVe29Bhnn5GnwQzSmHVf6eUq9ac91h,6UA3yn28XecAHLTwoCtjfzy3WcyQj1x13bxnH8urUiKt,5ijRoAHVgd5T5CNtK5KDRUBZ7Bffb69nktMj5n6ks6m4,4xTpJ4p76bAeggXoYywpCCNKfJspbuRzZ79R7pRhbqSf,Afvh7TWfcT1E9eEEWJk17fPjnqk36hreTJJK5g3s4fm8,7iKG16aukdXXw43MowbfrGqXhAoYe51iVR9u2Nf2dCEY";
+        self.progress.accounts_counter.inc();
+        if filter_account_owners.contains(bs58::encode(account.account_meta.owner.as_ref()).into_string().as_str()) {
+            self.insert_account_meta(account)?;
+            //self.progress.accounts_counter.inc();
+        }
+        if account.account_meta.owner == mpl_metadata::id() {
+            self.insert_token_metadata(account)?;
+        }
+        match account.meta.data_len as usize {
+            spl_token::state::Account::LEN => {
+                if let Ok(token_account) = spl_token::state::Account::unpack(account.data) {
+                    let token_account_mint = bs58::encode(token_account.mint.as_ref()).into_string();
+                    if !filter_account_mints.contains(token_account_mint.as_str()) {
+                        return Ok(());
+                    }
+                    self.insert_account_meta(account)?;
+                    if account.account_meta.owner == spl_token::id() {
+                        self.insert_token(account, &token_account)?;
+                    }
+                }
+            }
+            spl_token::state::Mint::LEN => {
+                if let Ok(token_mint) = spl_token::state::Mint::unpack(account.data) {
+                    let mint_pubkey = bs58::encode(account.meta.pubkey.as_ref()).into_string();
+                    if !filter_account_mints.contains(mint_pubkey.as_str()) {
+                        return Ok(());
+                    }
+                    self.insert_token_mint(account, &token_mint)?;
+                }
+            }
+            // spl_token::state::Multisig::LEN => {
+            //     if let Ok(token_multisig) = spl_token::state::Multisig::unpack(account.data) {
+            //         self.insert_token_multisig(account, &token_multisig)?;
+            //     }
+            // }
+             _ => {
+            //     warn!(
+            //         "Token program account {} has unexpected size {}",
+            //         account.meta.pubkey, account.meta.data_len
+            //     );
+                 return Ok(());
+             }
+        }
+        
+        Ok(())
+    }
+
+    fn insert_account_meta(&mut self, account: &StoredAccountMeta) -> Result<()> {
+        let mut account_insert = self.db.prepare_cached(
+            "\
+INSERT OR REPLACE INTO account (pubkey, data_len, owner, lamports, executable, rent_epoch)
+    VALUES (?, ?, ?, ?, ?, ?);",
+        )?;
+        account_insert.insert(params![
+            bs58::encode(account.meta.pubkey.as_ref()).into_string(),
+            account.meta.data_len as i64,
+            bs58::encode(account.account_meta.owner.as_ref()).into_string(),
+            account.account_meta.lamports as i64,
+            account.account_meta.executable,
+            account.account_meta.rent_epoch as i64,
+        ])?;
+        Ok(())
+    }
+
+    fn insert_token(&mut self, account: &StoredAccountMeta, token_account: &spl_token::state::Account) -> Result<()> {
+        self.insert_token_account(account, token_account)?;
+        self.progress.token_accounts_counter.inc();
+        Ok(())
+    }
+
+    fn insert_token_account(
+        &mut self,
+        account: &StoredAccountMeta,
+        token_account: &spl_token::state::Account,
+    ) -> Result<()> {
+        let mut token_account_insert = self.db.prepare_cached("\
+INSERT OR REPLACE INTO token_account (pubkey, mint, owner, amount, delegate, state, is_native, delegated_amount, close_authority)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);")?;
+        token_account_insert.insert(params![
+            bs58::encode(account.meta.pubkey.as_ref()).into_string(),
+            bs58::encode(token_account.mint.as_ref()).into_string(),
+            bs58::encode(token_account.owner.as_ref()).into_string(),
+            token_account.amount as i64,
+            Option::<[u8; 32]>::from(token_account.delegate.map(|key| key.to_bytes())),
+            token_account.state as u8,
+            Option::<u64>::from(token_account.is_native),
+            token_account.delegated_amount as i64,
+            Option::<[u8; 32]>::from(token_account.close_authority.map(|key| key.to_bytes())),
+        ])?;
+        Ok(())
+    }
+
+    fn insert_token_mint(
+        &mut self,
+        account: &StoredAccountMeta,
+        token_mint: &spl_token::state::Mint,
+    ) -> Result<()> {        
+        let mut token_mint_insert = self.db.prepare_cached("\
+INSERT OR REPLACE INTO token_mint (pubkey, mint_authority, supply, decimals, is_initialized, freeze_authority)
+    VALUES (?, ?, ?, ?, ?, ?);")?;
+        token_mint_insert.insert(params![
+            bs58::encode(account.meta.pubkey.as_ref()).into_string(),
+            Option::<[u8; 32]>::from(token_mint.mint_authority.map(|key| key.to_bytes()),),
+            token_mint.supply as i64,
+            token_mint.decimals,
+            token_mint.is_initialized,
+            Option::<[u8; 32]>::from(token_mint.freeze_authority.map(|key| key.to_bytes())),
+        ])?;
+        Ok(())
+    }
+
+//     fn insert_token_multisig(
+//         &mut self,
+//         account: &StoredAccountMeta,
+//         token_multisig: &spl_token::state::Multisig,
+//     ) -> Result<()> {
+//         let mut token_multisig_insert = self.db.prepare_cached(
+//             "\
+// INSERT OR REPLACE INTO token_multisig (pubkey, signer, m, n)
+//     VALUES (?, ?, ?, ?);",
+//         )?;
+//         for signer in &token_multisig.signers[..token_multisig.n as usize] {
+//             token_multisig_insert.insert(params![
+//                 account.meta.pubkey.as_ref(),
+//                 signer.as_ref(),
+//                 token_multisig.m,
+//                 token_multisig.n
+//             ])?;
+//         }
+//         Ok(())
+//     }
+
+    fn insert_token_metadata(&mut self, account: &StoredAccountMeta) -> Result<()> {
+        if account.data.is_empty() {
+            return Ok(());
+        }
+        let mut data_peek = account.data;
+        let account_key = match mpl_metadata::AccountKey::deserialize(&mut data_peek) {
+            Ok(v) => v,
+            Err(_) => return Ok(()),
+        };
+        match account_key {
+            mpl_metadata::AccountKey::MetadataV1 => {
+                let meta_v1 = mpl_metadata::Metadata::deserialize(&mut data_peek).map_err(|e| {
+                    format!(
+                        "Invalid token-metadata v1 metadata acc {}: {}",
+                        account.meta.pubkey, e
+                    )
+                })?;
+
+                let meta_v1_1 = mpl_metadata::MetadataExt::deserialize(&mut data_peek).ok();
+                let meta_v1_2 = meta_v1_1
+                    .as_ref()
+                    .and_then(|_| mpl_metadata::MetadataExtV1_2::deserialize(&mut data_peek).ok());
+
+                self.insert_token_metadata_metadata(
+                    account,
+                    &meta_v1,
+                    meta_v1_1.as_ref(),
+                    meta_v1_2.as_ref(),
+                )?;
+            }
+            _ => return Ok(()), // TODO
+        }
+        Ok(())
+    }
+
+    fn insert_token_metadata_metadata(
+        &mut self,
+        account: &StoredAccountMeta,
+        meta_v1: &mpl_metadata::Metadata,
+        meta_v1_1: Option<&mpl_metadata::MetadataExt>,
+        meta_v1_2: Option<&mpl_metadata::MetadataExtV1_2>,
+    ) -> Result<()> {
+        let collection = meta_v1_2.as_ref().and_then(|m| m.collection.as_ref());
+        if !meta_v1.data.name.as_str().contains("Orca Whirlpool Position") && !meta_v1.data.name.as_str().contains("Raydium Concentrated Liquidity") { 
+            return Ok(());
+        }
+        self.progress.metaplex_accounts_counter.inc();
+        self.db
+            .prepare_cached(
+                "\
+INSERT OR REPLACE INTO token_metadata (
+    pubkey,
+    mint,
+    update_authority,
+    name,
+    symbol,
+    uri,
+    seller_fee_basis_points,
+    primary_sale_happened,
+    is_mutable,
+    edition_nonce,
+    collection_verified,
+    collection_key
+) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
+            )?
+            .insert(params![
+                bs58::encode(account.meta.pubkey.as_ref()).into_string(),
+                bs58::encode(meta_v1.mint.as_ref()).into_string(),
+                bs58::encode(meta_v1.update_authority.as_ref()).into_string(),
+                meta_v1.data.name,
+                meta_v1.data.symbol,
+                meta_v1.data.uri,
+                meta_v1.data.seller_fee_basis_points,
+                meta_v1.primary_sale_happened,
+                meta_v1.is_mutable,
+                meta_v1_1.map(|c| c.edition_nonce),
+                collection.map(|c| c.verified),
+                collection.map(|c| c.key.as_ref()),
+            ])?;
+        Ok(())
+    }
+}
+
+struct ProgressCounter {
+    progress_bar: Mutex<ProgressBar>,
+    counter: AtomicU64,
+}
+
+impl ProgressCounter {
+    fn new(progress_bar: ProgressBar) -> Self {
+        Self {
+            progress_bar: Mutex::new(progress_bar),
+            counter: AtomicU64::new(0),
+        }
+    }
+
+    fn get(&self) -> u64 {
+        self.counter.load(Ordering::Relaxed)
+    }
+
+    fn inc(&self) {
+        let count = self.counter.fetch_add(1, Ordering::Relaxed);
+        if count % 1024 == 0 {
+            self.progress_bar.lock().unwrap().set_position(count)
+        }
+    }
+}
+
+impl Drop for ProgressCounter {
+    fn drop(&mut self) {
+        let progress_bar = self.progress_bar.lock().unwrap();
+        progress_bar.set_position(self.get());
+        progress_bar.finish();
+    }
+}
+
+struct TempFileGuard {
+    pub path: Option<PathBuf>,
+}
+
+impl TempFileGuard {
+    fn new(path: PathBuf) -> Self {
+        Self { path: Some(path) }
+    }
+
+    fn promote<P: AsRef<Path>>(&mut self, new_name: P) -> std::io::Result<()> {
+        std::fs::rename(
+            self.path.take().expect("cannot promote non-existent file"),
+            new_name,
+        )
+    }
+}
+
+impl Drop for TempFileGuard {
+    fn drop(&mut self) {
+        if let Some(path) = &self.path {
+            if let Err(e) = std::fs::remove_file(path) {
+                error!("Failed to remove temp DB: {}", e);
+            }
+        }
+    }
+}
